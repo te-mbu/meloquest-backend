e70e1ff9fa033554f70d762aa0de78d25e7ebc42
Terence Mbu <mbu.terence@gmail.com>
2023-05-01 11:54:04 +0200

    clean

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eventsRouter = require('./routes/events')
 var sendPicRouter = require('./routes/sendPic')
@@ -26,7 +25,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/events', eventsRouter);
 app.use('/sendPic', sendPicRouter);
